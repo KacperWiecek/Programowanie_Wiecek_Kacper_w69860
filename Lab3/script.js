a89eae291f46ab799501e6529b89a3bc764d6470
KacperWiecek <166850193+KacperWiecek@users.noreply.github.com>
2024-04-25 17:39:17 +0200

    Zadania do LAB3

---
+++ Lab3/script.js
@@ -0,0 +1,71 @@
+// alert("Hello");
+// let a=10;
+// let b=20;
+// let c=23.2
+
+// alert(a+b+c)
+// alert(c-a)
+// alert(a*b)
+// alert(b/a)
+
+// let wynik = a+b+c;
+
+// document.getElementById("Zad 3").innerHTML="wynik dodawania to: "+ wynik;
+
+// //zad 4
+
+// for(let i =0; i<101; i++){
+//     if(i%2==0){
+//         console.log(i);
+//      }
+//  }
+
+// //zad 5
+// let d,e,f ,p ,pole;
+// d = parseFloat(prompt("Podaj długośc boku1"));
+// e= parseFloat(prompt("Podaj długośc boku2"));
+// f = parseFloat(prompt("Podaj długośc boku3"));
+
+// console.log(d);
+
+// p=(d+e+f)/2;
+// console.log(p);
+// console.log(p*(p-d)*(p-e)*(p-f));
+
+// pole=Math.sqrt(p*(p-d)*(p-e)*(p-f));
+// console.log(pole);
+
+// //zad 6
+
+// let name = prompt("Podaj swoje imię:");
+// alert("Witaj " + name + "!");
+        
+  
+
+
+// zad 7
+// let a = parseFloat(prompt("Podaj pierwszą liczbę całkowitą:"));
+// let b = parseFloat(prompt("Podaj drugą liczbę całkowitą:"));
+// let wynik = a + b;
+// alert("Wynik dodawania: " + wynik);
+
+// zad8
+// let a = parseFloat(prompt("Podaj pierwszą liczbę:"));
+// let b = parseFloat(prompt("Podaj drugą liczbę:"));
+// let c = parseFloat(prompt("Podaj trzecią liczbę:"));
+// let max = Math.max(a, b, c);
+// alert("Największa liczba: " + max);
+
+// const a = parseInt(prompt("Podaj pierwszą liczbę całkowitą: "));
+// const b = parseInt(prompt("Podaj drugą liczbę całkowitą: "));
+
+// let c = a;
+// let d = b;
+
+// while (d !== 0) {
+//     let t = d;
+//     d = c % d;
+//     c = t;
+// }
+
+// console.log("Największy wspólny dzielnik liczb " + a + " i " + b + " to: " + c);
